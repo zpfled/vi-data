2d6d9dc37a59cb57ff549c09bc6330925ed55ff2
Tyler Leman <tyler.leman@gmail.com>
2018-02-26 23:19:04 -0600

    play-by-play url has correct home-away ordering

--- src/utils/getPlayByPlay.js
+++ src/utils/getPlayByPlay.js
@@ -4,14 +4,12 @@ let cheerio = require('cheerio');
 let moment = require('moment');
 let numeral = require('numeral');
 
-let homeTeam = 'illinois'
-let scheduleUrl = ['https://www.ncaa.com/schools', homeTeam, 'basketball-men'].join('/')
+let thisTeamUrlName = 'illinois'
+let scheduleUrl = ['https://www.ncaa.com/schools', thisTeamUrlName, 'basketball-men'].join('/')
 
 request(scheduleUrl, function (err, response, html) {
     if (err) { return console.log(err); }
-    // console.log('body:', html);
-    // console.log('type:', typeof(html));
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('Read HTML from ', scheduleUrl); // print the response status code if a response was received
 
     // load html
     const $ = cheerio.load(html)
@@ -19,37 +17,49 @@ request(scheduleUrl, function (err, response, html) {
 
     // scan all table row elements
     $('tr').each( function(i, element) {
-        // if this row has first td tag matching a date
         let dateFromHtml = $(this).children().eq(0).text()
-        let schoolUrlFromHtml = $(this).children().eq(1).children().eq(0).attr('href')
+        let opponentUrlFromHtml = $(this).children().eq(1).children().eq(0).attr('href')
         let date = moment(dateFromHtml, 'MM/DD/YY', true)
         if (date.isValid()) {
-            // console.log('i:', i);
-            // console.log('dateFromHtml :', dateFromHtml);
-            // console.log('schoolUrlFromHtml :', schoolUrlFromHtml);
-            // console.log('date :', date);
-            // console.log('year :', date.year());
-            // console.log('month :', date.month()+1);
-            // console.log('day :', date.date());
-
-            baseUrl = 'https://data.ncaa.com/jsonp/game/basketball-men/d1'
+            // define the url that has the play-by-play json
             year = numeral(date.year()).format('0000')
             month = numeral(date.month()+1).format('00')
             day = numeral(date.date()).format('00')
-            opponent = schoolUrlFromHtml.split('/').pop()
-
-            awayTeam = opponent // will fix this
-            let urlArray = [baseUrl, year, month, day, awayTeam + '-' + homeTeam, 'pbp.json'];
-            let url = urlArray.join('/')
+            opponentUrlName = opponentUrlFromHtml.split('/').pop()
+            opponentText = $(this).children().eq(1).text()
+            if (opponentText[0] != '@') {
+                homeTeam = thisTeamUrlName
+                awayTeam = opponentUrlName
+            }
+            else {
+                homeTeam = opponentUrlName
+                awayTeam = thisTeamUrlName
+            }
+            let pbpUrlArray = ['https://data.ncaa.com/jsonp/game/basketball-men/d1', year, month, day, awayTeam + '-' + homeTeam, 'pbp.json'];
+            let pbpUrl = pbpUrlArray.join('/')
 
             // if game took place before now, request the play-by-play json from each URL
             if (date.isBefore(moment())) {
-                request(url, { json: true }, (err, response, body) => {
-                if (err) { return console.log(err); }
-                    // console.log('body:', body);
-                    // console.log('type:', typeof(body));
-                    console.log('Requesting play-by-play json from: ', url)
-                    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                request(pbpUrl, { json: true }, (err, response, body) => {
+                    if (err) { return console.log(err); }
+                    // if (response.statusCode == 404) {
+                    //     console.log('Requested play-by-play json from: ', pbpUrl) 
+                    //     console.log('status code: ', response.statusCode)
+                    //     console.log('Attempting to increment date by one day...')
+                    //     day = numeral(date.date()+1).format('00')
+                    //     pbpUrlArray = ['https://data.ncaa.com/jsonp/game/basketball-men/d1', year, month, day, awayTeam + '-' + homeTeam, 'pbp.json'];
+                    //     pbpUrl = pbpUrlArray.join('/')
+                    //     request(pbpUrl, { json: true }, (err, response, body) => {
+                    //         console.log('Requested play-by-play json from: ', pbpUrl)
+                    //         console.log('status code: ', response.statusCode, '\n')
+                    //     });
+                    // }
+                    // else {
+                    //     console.log('Requested play-by-play json from: ', pbpUrl)    
+                    //     console.log('status code: ', response.statusCode, '\n')
+                    // };
+                    console.log('Requested play-by-play json from: ', pbpUrl)    
+                    console.log('status code: ', response.statusCode, '\n')
                 });
             }
         }
